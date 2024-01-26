11abb6262f5ab2263063d2c11ce80a7589cea479
Arman Grema <abdurrahmangrema19@gmail.com>
2024-01-25 20:31:57 -0800

    added account creation

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express'),
       app = express(),
-      dotenv = require('dotenv').config()
+      dotenv = require('dotenv').config(),
+      payvassel = require("./payvessel/index")
+      // Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
     console.log(process.env.PAYVASSEL_URL)
@@ -8,5 +14,11 @@ app.get('/', (req,res)=>{
 })
 
 // creating virtual reserve account for wallets
+
+app.post("/create/account",async(req,res)=>{
+    const {email, phoneNumber, name} = req.body;
+const response = await payvassel.createAccount({email, name, phoneNumber})
+res.json({responseObj:response})
+})
 
 app.listen(2000, console.log("server is listen on port 2000"))
---
+++ payvessel/createAccount.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+module.exports = async (payload) => {
+    console.log(process.env.PAYVASSEL_URL)
+    const { email, name, phoneNumber } = payload
+    const data = {
+        email,
+        name,
+        phoneNumber,
+        "bankcode": ["101", "120001"],
+        "account_type": "STATIC",
+        "businessid": process.env.PAYVASSEL_BUSSINESS_ID,
+        "bvn": "",
+
+    }
+    const header = {
+        'api-key': process.env.PAYVASSEL_API_KEY,
+        'api-secret':`Bearer ${process.env.PAYVASSEL_SEC_KEY}`,
+        'Content-Type': 'application/json'
+    }
+    const request = await axios.post(`${process.env.PAYVASSEL_URL}api/external/request/customerReservedAccount/`,
+    data,{header}
+)
+    // console.log(request)
+    return request
+}
