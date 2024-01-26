b0e24c470d702b8e4a876449b37906e84a4aff3b
Arman Grema <abdurrahmangrema19@gmail.com>
2024-01-25 19:08:37 -0800

    configure dotenv

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express'),
-      app = express();
+      app = express(),
+      dotenv = require('dotenv').config()
 
 app.get('/', (req,res)=>{
+    console.log(process.env.PAYVASSEL_URL)
     res.send("<h1>hello brother</h1>")
 })
+
+// creating virtual reserve account for wallets
 
 app.listen(2000, console.log("server is listen on port 2000"))
